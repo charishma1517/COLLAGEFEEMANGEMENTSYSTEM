4f9a4c152c5823a5aff380afb24c3a12c57a6fb4
charishma1517 <153185496+charishma1517@users.noreply.github.com>
2025-05-29 22:59:02 +0530

    Create  ReportCriteriaServlet.java

---
+++ com/servlet/ReportCriteriaServlet.java
@@ -0,0 +1,13 @@
+package com.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@WebServlet("/ReportCriteriaServlet")
+public class ReportCriteriaServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("report_form.jsp").forward(request, response);
+    }
+}
